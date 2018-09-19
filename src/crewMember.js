e4c31ea6d7b70e02c79391b81465072d83508f93
Yixuan Wang <ywang0506@gmail.com>
2018-09-19 15:05:23 -0400

    Done.

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,40 @@
-class CrewMember {}
+class CrewMember {
+  constructor(position){
+    this.position = position
+    this.currentShip = "Looking for a Rig"
+
+  }
+  currentShip(ship){
+    this.currentShip =ship
+  }
+  engageWarpDrive(){
+    if (this.currentShip === "Looking for a Rig") {
+      return "had no effect"
+    } else {
+      if (this.position === "pilot"){
+        this.currentShip.warpDrive ='engaged'
+
+      } else {
+        this.currentShip.warpDrive ='disengaged'
+      }
+    }
+  }
+  setsInvisibility(){
+    if (this.currentShip === "Looking for a Rig") {
+      return "had no effect"
+    } else {
+      if(this.position === "Defender"){
+        this.currentShip.cloaked =true
+      } else {
+        this.currentShip.cloaked =false;
+      
+      }
+     }
+  }
+  chargePhasers(){
+    if (this.currentShip === "Looking for a Rig") {
+      return "had no effect"
+    }
+  }
+
+}
--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,21 @@
-class Spaceship {}
+class Spaceship {
+  constructor(name, crew,phasers,shields){
+    this.name =name
+    this.phasers = phasers
+    this.shields= shields
+    this.cloaked = false
+    this.crew = crew
+    this.warpDrive = 'disengaged'
+    if (crew.length === 0) {
+      this.docked = true
+
+    } else {
+      this.docked = false
+      for (let ele of crew){
+        ele.currentShip = this
+
+      }
+    }
+    this.phasersCharge = 'uncharged'
+  }
+}
